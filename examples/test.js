5291839cfdb1caa793bd3b70c1621b5ba9a95bc3
lilindog <lilin@lilin.site>
2021-03-21 16:38:59 +0800

    修改初始化型号校验，增加命令行搜索微光设备

--- examples/test.js
+++ examples/test.js
@@ -1,17 +1,11 @@
 const VguangScanner = require("../VguangScanner");
 
-const scanner = new VguangScanner({ mode: "tx200" });
+const scanner = new VguangScanner({ mode: "tx400" });
 
-// scanner.beep(10);
-// scanner.blinkGreen(10, 1000)
-// scanner.toggleLight(false);
-setTimeout(() => {
-    scanner.toggleLight(true);
-}, 2000);
-let num = 0;
-scanner.on("data", code => {
-    console.log("数据：");
-    console.log(code);
-    scanner.beep(1, 20);
-    num++;
-    console.log(`第${num
+scanner.on("data", data => {
+    console.log(data);
+});
+
+setInterval(() => {
+    scanner.beep();
+}, 1000);
