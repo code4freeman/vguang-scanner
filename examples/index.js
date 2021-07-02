62c3ba7a99f8947a9ef8c397c3d9550685688378
lilindog <lilin@lilin.site>
2021-07-02 13:55:02 +0800

    修改examples

--- examples/index.js
+++ examples/index.js
@@ -2,24 +2,13 @@ const VguangScanner = require("../src/VguangScanner");
 
 const scanner = new VguangScanner({ mode: "tx200" });
 
-const stack = [];
-const _push = stack.push;
-stack.push = function (...args) {
-    _push.call(stack, ...args);
-    if (this.length > 2) this.splice(0, this.length - 2);
-}
+let is = true;
 
 scanner.on("data", data => {
-    if (!stack.includes(data)) {
-        scanner.beep();
-        stack.push(data);
-    } 
-    console.log(stack);
+    if (is) {
+        is = false;
+        console.log(data);
+        scanner.beep(1);
+        setTimeout(() => is = true, 800);
+    }
 });
-
-// scanner.toggleLight(false);
-
-setInterval(() => {
-    // scanner.beep();
-    scanner.blinkRed(3, 200);
-}, 2000);
